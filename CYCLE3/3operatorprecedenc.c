92546cec541f7e8e2af8f390691e523399cfa3c5
Elizabeth <elizabethmariyashibu2004@gmail.com>
2025-10-28 18:52:54 +0530

    added

--- CYCLE3/3operatorprecedenc.c
+++ CYCLE3/3operatorprecedenc.c
@@ -1,99 +1,79 @@
-CODE: 
-#include <stdio.h> 
-#include <string.h> 
-#define MAX 50 
- 
-char stack[MAX], input[MAX]; 
-char table[10][10]; 
-char terminals[10]; 
-int n; 
- 
-int idx(char symbol) { 
-    for (int i = 0; i < n; i++) { 
-        if (terminals[i] == symbol) 
-            return i; 
-    } 
-    return -1; 
-} 
- 
-int main() { 
-    int i, j, top = 0; 
-    char temp[MAX], a, b; 
-    int col, row; 
- 
-    printf("Enter number of terminals: "); 
-    scanf("%d", &n); 
-    printf("Enter the terminals (single characters like + * i $):\n"); 
-    scanf("%s", terminals); 
-    printf("\nEnter operator precedence table values (<, >, =):\n"); 
-    for (i = 0; i < n; i++) { 
-        for (j = 0; j < n; j++) { 
-            printf("Enter value for %c %c: ", terminals[i], terminals[j]); 
-            scanf(" %c", &table[i][j]); 
-        } 
-    } 
- 
-    printf("\n**** OPERATOR PRECEDENCE TABLE ****\n\t"); 
-    for (i = 0; i < n; i++) 
-        printf("%c\t", terminals[i]); 
-    printf("\n"); 
-    for (i = 0; i < n; i++) { 
-        printf("%c\t", terminals[i]); 
-        for (j = 0; j < n; j++) 
-            printf("%c\t", table[i][j]); 
-        printf("\n"); 
-    } 
-    printf("\nEnter the input string (e.g., id+id*id): "); 
-    scanf("%s", temp); 
- 
-    int k = 0, m = 0; 
-    while (temp[k] != '\0') { 
-        if (temp[k] == 'i' && temp[k + 1] == 'd') { 
-            input[m++] = 'i'; 
-            k += 2; 
-        } else { 
-            input[m++] = temp[k++]; 
-        } 
-    } 
-    input[m++] = '$'; 
-    input[m] = '\0'; 
- 
-    stack[top] = '$'; 
-    printf("\nSTACK\t\tINPUT\t\tACTION\n"); 
-    printf("%s\t\t%s\t", stack, input); 
- 
-    i = 0; 
-    while (1) { 
-        a = stack[top]; 
-        b = input[i]; 
-        col = idx(a); 
-        row = idx(b); 
-        if (col == -1 || row == -1) { 
-            printf("\nError: invalid symbol.\n"); 
-            break; 
-        } 
-        if (a == '$' && b == '$') { 
-            printf("\nString Accepted.\n"); 
-            break; 
-        } else if (table[col][row] == '<' || table[col][row] == '=') { 
-            stack[++top] = table[col][row]; 
-            stack[++top] = b; 
-            printf("Shift %c\n", b); 
-            i++; 
-        } else if (table[col][row] == '>') { 
-            while (stack[top] != '<') 
-                top--; 
-            top--; 
-            printf("Reduce\n"); 
-        } else { 
-            printf("\nError: string not accepted.\n"); 
-            break; 
-        } 
-        printf("%s\t\t", stack); 
-        for (int p = i; p < strlen(input); p++) 
-            printf("%c", input[p]); 
-        printf("\t"); 
-    } 
- 
-    return 0; 
-} 
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+char stack[MAX], input[MAX], terminals[100], table[10][10];
+int n;
+
+int idx(char symbol) {
+    for (int i = 0; i < n; i++) {
+        if (terminals[i] == symbol)
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    int top = 0, i = 0;
+    printf("Enter number of terminals: ");
+    scanf("%d", &n);
+
+    printf("Enter the terminals (no spaces, e.g. i+*()$): ");
+    scanf("%s", terminals);
+
+    printf("\nEnter operator precedence table (<, >, =):\n");
+    for (int r = 0; r < n; r++) {
+        for (int c = 0; c < n; c++) {
+            printf("%c vs %c: ", terminals[r], terminals[c]);
+            scanf(" %c", &table[r][c]);  // leading space to skip newlines
+        }
+    }
+
+    printf("\nEnter input string: ");
+    scanf("%s", input);
+    strcat(input, "$"); // append end marker
+
+    stack[top] = '$';
+    stack[top + 1] = '\0';
+
+    printf("\nSTACK\t\tINPUT\t\tACTION\n");
+
+    while (1) {
+        char a = stack[top];  // top of stack
+        char b = input[i];   // next input symbol
+
+        int row = idx(a);
+        int col = idx(b);
+
+        if (row == -1 || col == -1) {
+            printf("Error: invalid symbol.\n");
+            break;
+        }
+
+        if (a == '$' && b == '$') {
+            printf("%s\t\t%s\t\tAccept\n", stack, input + i);
+            break;
+        }
+
+        char relation = table[row][col];
+        if (relation == '<' || relation == '=') {
+            stack[++top] = relation;
+            stack[++top] = b;
+            stack[top + 1] = '\0';
+            printf("%s\t\t%s\t\tShift %c\n", stack, input + i, b);
+            i++;
+        } else if (relation == '>') {
+            while (stack[top] != '<' && top > 0)
+                top--;
+            if (top > 0)
+                top--;
+            stack[top + 1] = '\0';
+            printf("%s\t\t%s\t\tReduce\n", stack, input + i);
+        } else {
+            printf("%s\t\t%s\t\tError (no relation)\n", stack, input + i);
+            break;
+        }
+    }
+
+    return 0;
+}
